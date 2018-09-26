f182c0f3b1dd92834b0f341a108bab45d43519d8
Eduardo Acacio <acaciosh@gmail.com>
2018-09-26 20:23:46 -0300

    Realizando testes e adicionando repository pattern

---
+++ src/main/java/com/cpqi/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.cpqi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cpqi.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}

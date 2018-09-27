ae0e64a4fd90652cd40444b072a5995e1311ac83
Eduardo Acacio <acaciosh@gmail.com>
2018-09-27 13:46:48 -0300

    adicionando estado e cidade

---
+++ src/main/java/com/cpqi/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.cpqi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cpqi.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+}

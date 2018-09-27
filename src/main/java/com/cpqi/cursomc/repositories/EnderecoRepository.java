35674e569777f6918a009a9ed1f2eb98e4de218d
Eduardo Acacio <acaciosh@gmail.com>
2018-09-27 15:48:06 -0300

    Endpoint /clientes/{id}

---
+++ src/main/java/com/cpqi/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.cpqi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cpqi.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+	
+}
